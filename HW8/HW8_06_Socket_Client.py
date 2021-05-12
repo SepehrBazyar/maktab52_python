0f291613b631a37172e08514e3b9863b98c8069f
SepehrBazyar <bazyar.sepehr@gmail.com>
2021-05-12 16:37:25 +0430

    Completed Version 1 of Translator Socket Program

--- HW8/HW8_06_Socket_Client.py
+++ HW8/HW8_06_Socket_Client.py
@@ -1,8 +1,27 @@
 # Written by: Sepehr Bazyar
-import socket
-import logging
-import pickle
+import socket, logging, pickle
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(levelname)-10s - %(message)s')
+logging.basicConfig(level = logging.INFO, format = '%(asctime)s - %(levelname)-10s - %(message)s')
 
+host, port = socket.gethostname(), 9999
+tcpsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = (host, port)
+tcpsocket.connect(server_address)
+logging.info("Connected to the Server.")
+
+provider = input("Choose Your Provider[Google or Bing]? ").strip().lower()
+assert provider in ['google', 'bing'], "Invalid Provider for Translating!"
+target_lang = input("Please Select Target Language -> ").strip().lower()[: 2]
+start_lang = input("Enter Your Language Text(Enter for Auto): ").strip().lower()[: 2] or "auto"
+
+while True:
+    message = input("Text(Just Enter Exit Program)>>> ")
+    tcpsocket.send(pickle.dumps([message, provider, target_lang, start_lang]))
+    logging.debug("Successfully Send Text Message to Server for Translating.")
+    if message:
+        print(tcpsocket.recv(1024).decode())
+        logging.debug("Successfully Get Translated Text Message from Server.")
+    else:
+        logging.warning("Quit at Translator Program Server!")
+        tcpsocket.close()
+        break
--- HW8/HW8_06_Socket_Server.py
+++ HW8/HW8_06_Socket_Server.py
@@ -1,35 +1,31 @@
 # Written by: Sepehr Bazyar
-import socket
-import logging
-import pickle
-import translators
+import socket, logging, pickle, translators
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(levelname)-10s - %(message)s')
+logging.basicConfig(level = logging.DEBUG, format = '%(asctime)s - %(levelname)-10s - %(message)s')
 
-# localhost is 127.0.0.1 IP(this PC) and 9999 desire port(upper 8000)
-host, port = "localhost", 9999
-# AF_INET IPv4 and SOCK_STREAM TCP protocol connection
-tcpsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host, port = "", 9999 #localhost is 127.0.0.1 IP(this PC) and 9999 desire port(upper 8000)
+tcpsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_INET IPv4 SOCK_STREAM TCP protocol
 server_address = (host, port)
 tcpsocket.bind(server_address)
 logging.info("Server Created Successfully.")
 
-logging.info("Waiting for a Connection...")
-tcpsocket.listen(1)  # waiting for connection from 1 cleint request
+logging.debug("Waiting for a Connection...")
+tcpsocket.listen(1) #waiting for connection from 1 cleint request
 
 client, addr = tcpsocket.accept()
 logging.info(f"{addr[0]} IP at {addr[1]} Port Connected!")
 
 while True:
     data = pickle.loads(client.recv(1024))
-    if data:
-        logging.info("Get a Text Message!")
-        logging.info("Translating...")
-        text = getattr(translators, data[1])(
-            data[0], to_language=data[2], from_language=data[3])
-        client.send(text.encode())
-        logging.info("Send Translation for Client!")
+    logging.info("Get a Text Message!")
+    if data[0]:
+        logging.debug("Translating...")
+        try:
+            text = getattr(translators, data[1])(data[0],
+            to_language = data[2], from_language = data[3])
+            client.send(text.encode())
+            logging.info("Send Translation for Client!")
+        except: logging.error("Invalid Text! Try Again...")
     else:
         logging.warning("Client Disconnected!")
         tcpsocket.close()
