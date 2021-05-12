7c3a3e3ba39a2db2f12c58f18b2da971046e0557
SepehrBazyar <bazyar.sepehr@gmail.com>
2021-05-12 14:29:21 +0430

    Split Program to 2 Code and Writing Server Code

---
+++ HW8/HW8_06_Socket_Client.py
@@ -0,0 +1,8 @@
+# Written by: Sepehr Bazyar
+import socket
+import logging
+import pickle
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)-10s - %(message)s')
+
