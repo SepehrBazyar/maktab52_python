6d9bf6f1c45e256f98f9a023cb6c9075d5ca1363
SepehrBazyar <bazyar.sepehr@gmail.com>
2021-05-12 19:13:19 +0430

    Add Option for Change Setting etc Provider or ...

--- HW8/HW8_06_Socket_Client.py
+++ HW8/HW8_06_Socket_Client.py
@@ -1,31 +1,41 @@
 # Written by: Sepehr Bazyar
-import socket, logging, pickle
+import socket, logging, pickle #translators module in server code
 
 logging.basicConfig(level = logging.INFO, format = '%(asctime)s - %(levelname)-10s - %(message)s')
 
-host, port = socket.gethostname(), 9999
+host, port = socket.gethostname(), 9999 #gethostname refrence to pc name or localhost
 tcpsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = (host, port)
-tcpsocket.connect(server_address)
+tcpsocket.connect(server_address) #connecting to server
 logging.info("Connected to the Server.")
 
-provider = input("Choose Your Provider[Google or Bing]? ").strip().lower()
-while provider not in ('google', 'bing'):
+print("Welcome to My Translator Program ^_^\t{© Seper Bazyar}\n")
+provider = input("Choose Your Provider[Google or Bing]? ").strip().lower() #choose translator
+while provider not in ('google', 'bing'): #while provider != 'google' and provider != 'bing': ...
     print("Invalid Provider for Translating! Please Try Again...")
     provider = input("Choose Your Provider[Google or Bing]? ").strip().lower()
-target_lang = input("Please Select Target Language -> ").strip().lower()[: 2]
+target_lang = input("Please Select Target Language -> ").strip().lower()[: 2] #just 2 first char
 start_lang = input("Enter Your Language Text(Enter for Auto): ").strip().lower()[: 2] or "auto"
 
-while True:
-    message = input("Text(Just Enter Exit Program)<<< ").strip()
-    tcpsocket.send(pickle.dumps([message, provider, target_lang, start_lang]))
-    logging.debug("Successfully Send Text Message to Server for Translating.")
-    if message:
-        trans = tcpsocket.recv(1024).decode()
-        logging.debug("Successfully Get Translated Text Message from Server.")
-        if trans != "#$%^&*~!@": print("Result Translated >>>", trans)
-        else: logging.error("Invalid Text! Please Try Again...")
-    else:
-        logging.warning("Quit at Translator Program Server!")
-        tcpsocket.close()
-        break
+while True: #infinity loop
+    message = input("Text(0 for Change Setting Just Enter Exit Program):\n<<< ").strip()
+    if message == '0': #setup config setting of system change languages or translator provider
+        print("\nPlease Just Enter for Unchanging Setting:\n") #just enter unchage value
+        provider = input("Choose Your Provider[Google or Bing]? ").strip().lower() or provider
+        while provider not in ('google', 'bing'):
+            print("Invalid Provider for Translating! Please Try Again...")
+            provider = input("Choose Your Provider[Google or Bing]? ").strip().lower() or provider
+        target_lang = input("Please Select Target Language -> ").strip().lower()[: 2] or target_lang
+        start_lang = input("Enter Your Language Text(Enter for Auto): ").strip().lower()[: 2] or start_lang
+    else: #if message not 0 is a text or emtpy string mean enter for the exit
+        tcpsocket.send(pickle.dumps([message, provider, target_lang, start_lang])) #send pickeling
+        logging.debug("Successfully Send Text Message to Server for Translating.")
+        if message:
+            trans = tcpsocket.recv(1024).decode() #get a binary string from the server for result
+            logging.debug("Successfully Get Translated Text Message from Server.")
+            if trans != "#$%^&*~!@": print("Result Translated:\n>>>", trans)
+            else: logging.error("Invalid Text! Please Try Again...") #error at translating
+        else: #message is '' that boolean value is false
+            logging.warning("Quit at Translator Program Server!")
+            tcpsocket.close() #disconnect to server and close program
+            break
--- HW8/HW8_06_Socket_Server.py
+++ HW8/HW8_06_Socket_Server.py
@@ -3,33 +3,33 @@ import socket, logging, pickle, translators
 
 logging.basicConfig(level = logging.DEBUG, format = '%(asctime)s - %(levelname)-10s - %(message)s')
 
-host, port = "", 9999 #localhost is 127.0.0.1 IP(this PC) and 9999 desire port(upper 8000)
+host, port = "", 9999 #"" or localhost is 127.0.0.1 IP(this PC) and 9999 desire port(upper 8000)
 tcpsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_INET IPv4 SOCK_STREAM TCP protocol
 server_address = (host, port)
-tcpsocket.bind(server_address)
+tcpsocket.bind(server_address) #create server
 logging.info("Server Created Successfully.")
 
 logging.debug("Waiting for a Connection...")
 tcpsocket.listen(1) #waiting for connection from 1 cleint request
 
-client, addr = tcpsocket.accept()
+client, addr = tcpsocket.accept() #addr is a tuple
 logging.info(f"{addr[0]} IP at {addr[1]} Port Connected!")
 
-while True:
-    data = pickle.loads(client.recv(1024))
+while True: #infinity loop
+    data = pickle.loads(client.recv(1024)) #get from the client(before convert from pickling)
     logging.info("Get a Text Message!")
-    if data[0]:
-        text = "#$%^&*~!@"
+    if data[0]: #data[0] is a user message can be a string to translate or empty to exit
+        text = "#$%^&*~!@" #a symbol for when error at translate so send for client
         logging.debug("Translating...")
         try:
-            text = getattr(translators, data[1])(data[0],
+            text = getattr(translators, data[1])(data[0], #use getattr func because providers
             to_language = data[2], from_language = data[3])
         except: logging.error("Invalid Text! Untranslatable...")
         else: logging.info("Send Translation for Client!")
-        finally: client.send(text.encode())
+        finally: client.send(text.encode()) #at last must be send a text to client finally
     else:
         logging.warning("Client Disconnected!")
-        tcpsocket.close()
+        tcpsocket.close() #close server and end program
         break
 
 logging.warning("Closing Server.")
