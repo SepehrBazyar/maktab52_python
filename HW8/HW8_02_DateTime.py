5d4a8a0cb273deda96a2f2b6095059945ff67501
SepehrBazyar <bazyar.sepehr@gmail.com>
2021-05-13 20:56:13 +0430

    Finish Part 3 Convert Jalali(Part 2 Not Started)

--- HW8/HW8_02_DateTime.py
+++ HW8/HW8_02_DateTime.py
@@ -5,4 +5,10 @@ first_datetime = datetime.datetime.strptime(input("YYYY/MM/DD - HH:MM:SS => "), 
 second_datetime = datetime.datetime.strptime(input("YYYY/MM/DD - HH:MM:SS => "), frmt)
 
 delta_datetime = abs(second_datetime - first_datetime)
-print("Duration Second is: ", int(delta_datetime.total_seconds()), 's', sep = '')
+print("\nDuration Second is: ", int(delta_datetime.total_seconds()), 's\n', sep = '')
+
+first_jdate = jdatetime.GregorianToJalali(first_datetime.year, first_datetime.month, first_datetime.day)
+second_jdate = jdatetime.GregorianToJalali(second_datetime.year, second_datetime.month, second_datetime.day)
+#using walrus operator for simultaneous assigmenting and printing(unpack jalali date list)
+print(first_datetime.date(), '=', first_jalali := jdatetime.date(*first_jdate.getJalaliList()))
+print(second_datetime.date(), '=', second_jalali := jdatetime.date(*second_jdate.getJalaliList()))
