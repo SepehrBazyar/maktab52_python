450e5fa968643da5798e7efe62575d02f3a2f6ba
SepehrBazyar <bazyar.sepehr@gmail.com>
2021-05-13 17:49:38 +0430

    Finished Question 3 and Comments at Test Cases

--- HW8/HW8_03_WeekDay_Gen.py
+++ HW8/HW8_03_WeekDay_Gen.py
@@ -1,27 +1,29 @@
-# Written by: Sepehr Bazyar
-from jdatetime import date
-from typing import Generator, Literal
+#Written by: Sepehr Bazyar
+from jdatetime import date, datetime #jalali datetime: date ad datetime to converting
+from typing import Generator, Literal #for highlighting input and output functions
 
-def normalize(day: date, w_day: Literal[0, 1, 2, 3, 4, 5, 6]) -> date:
+def normalize(day: date, w_day: Literal[0, 1, 2, 3, 4, 5, 6]) -> date: #fixing weekday date
     counter = day
-    while counter.weekday() != w_day:
-        try:
+    while counter.weekday() != w_day: #maximum 6 day to fixing
+        try: #next day
             counter = date(counter.year, counter.month, counter.day + 1)
         except:
-            try:
+            try: #next month(first day of next month)
                 counter = date(counter.year, counter.month + 1, 1)
-            except:
+            except: #next year(first day of first month new year)
                 counter = date(counter.year + 1, 1, 1)
     return counter
 
-def weekday_gen(s_day: date, e_day: date, w_day: Literal[0, 1, 2, 3, 4, 5, 6]) -> Generator:
-    counter = normalize(s_day, w_day)
-    while counter <= e_day:
-        yield counter
-        try:
+def weekday_gen(s_day: str, e_day: str, w_day: Literal[0, 1, 2, 3, 4, 5, 6]) -> Generator:
+    start = datetime.strptime(s_day, "%Y/%m/%d").date() #parse datetime from the string
+    end = datetime.strptime(e_day, "%Y/%m/%d").date() #convert to date for remove time
+    counter = normalize(start, w_day) #find starting day that right weekday
+    while counter <= end: #less than for date object is correct comparison
+        yield counter.strftime("%d %B %Y") #yield keyword generator(fix formatting)
+        try: #7 day later this week day in next week
             counter = date(counter.year, counter.month, counter.day + 7)
         except:
-            try:
+            try: #next month(because maybe 31 or 30 day can't use % operation)
                 counter = normalize(date(counter.year, counter.month + 1, 1), w_day)
             except:
                 counter = normalize(date(counter.year + 1, 1, 1), w_day)
