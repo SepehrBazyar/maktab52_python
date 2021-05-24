45cb0d80c4ffc0bf863596e51ffc901a37f8cdd8
SepehrBazyar <bazyar.sepehr@gmail.com>
2021-05-24 22:46:44 +0430

    Write Phone Validation and Alt+Shift+F for Cleaning

--- HW10/HW10_01_Validation.py
+++ HW10/HW10_01_Validation.py
@@ -4,15 +4,20 @@ import re as regex
 
 def name_validation(name: str) -> bool:
     "Lenght of Username Between 5 to 14 and Only has Char and Underline."
-    pattern = "^[A-Za-z\_]{5,14}$" # ^ for check in start string and $ for end of string
+    # ^ for check in start of string and $ for end of string
+    pattern = "^[A-Za-z\_]{5,14}$"
     return bool(regex.search(pattern, name))
 
 
 def email_validation(email: str) -> bool:
-    "name@mail.com is a Validate Email because has a Username and a Domin Name."
-    pattern = r"^([\w\.\_\-]+)[@]([\w\.\_\-]*\w)[.]([A-Za-z]{2,3})$" # split by group items
+    "name@mail.com is a Validate Email because has a Username and a Domain Name."
+    # split by group items and domain name musn't end with dot char
+    pattern = r"^([\w\.\_\-]+)[@]([\w\.\_\-]*\w)[.]([A-Za-z]{2,3})$"
     return bool(regex.search(pattern, email))
 
 
 def phone_validation(phone: str) -> bool:
-    pass
+    "Phone Number Must be Start with 09 or +989 and Lenght's is 11 or 13."
+    # after pre number each group 1 must be have 9 digits
+    pattern = r"^(09|\+989)(\d{9})$"
+    return bool(regex.search(pattern, phone))
