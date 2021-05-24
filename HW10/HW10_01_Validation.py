b4bc55a7cee8280a98a3dad4b45ce9cc8da1f87f
SepehrBazyar <bazyar.sepehr@gmail.com>
2021-05-24 21:23:29 +0430

    Create Directory Home Work 10 and Start Q1

---
+++ HW10/HW10_01_Validation.py
@@ -0,0 +1,14 @@
+# Written by: Sepehr Bazyar
+import re as regex
+
+
+def name_validation(name: str) -> bool:
+    pass
+
+
+def email_validation(email: str) -> bool:
+    pass
+
+
+def phone_validation(phone: str) -> bool:
+    pass
