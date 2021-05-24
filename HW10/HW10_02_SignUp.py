97d8e684506432387b187235e1f46aa6909b4d2b
SepehrBazyar <bazyar.sepehr@gmail.com>
2021-05-24 23:48:05 +0430

    Create User and Save to a Class Attr and save to json

--- HW10/HW10_02_SignUp.py
+++ HW10/HW10_02_SignUp.py
@@ -4,10 +4,23 @@ import json
 
 
 class SignUp:
+    USERS = []
+
     def __init__(self, name: str, phone: str, email: str) -> None:
         assert name_validation(name), "Just Char and Underline Between 5 & 14"
         assert phone_validation(phone), "Start with 09 or +989 and 9 Digits"
         assert email_validation(email), "Validate Sample: name@mail.com"
         self.name, self.phone, self.email = name, phone, email
+        self.USERS.append({'name': name, 'phone': phone, 'email': email})
 
-    
+    @classmethod
+    def save(cls):
+        with open("users.json", 'w') as fl:
+            json.dump(cls.USERS, fl)
+
+    def __repr__(self) -> str:
+        return f"""
+<Name:  {self.name}>
+<Phone: {self.phone}>
+<Email: {self.email}>
+"""
