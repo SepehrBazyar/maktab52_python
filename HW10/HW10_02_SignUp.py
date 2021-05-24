83bd4071fc74fff983a6b03390dc0e5c827738f9
SepehrBazyar <bazyar.sepehr@gmail.com>
2021-05-24 23:18:44 +0430

    import json and init user with check validated

--- HW10/HW10_02_SignUp.py
+++ HW10/HW10_02_SignUp.py
@@ -1,5 +1,13 @@
 # Written by: Sepehr Bazyar
 from HW10_01_Validation import name_validation, email_validation, phone_validation
+import json
+
 
 class SignUp:
-    pass
+    def __init__(self, name: str, phone: str, email: str) -> None:
+        assert name_validation(name), "Just Char and Underline Between 5 & 14"
+        assert phone_validation(phone), "Start with 09 or +989 and 9 Digits"
+        assert email_validation(email), "Validate Sample: name@mail.com"
+        self.name, self.phone, self.email = name, phone, email
+
+    
