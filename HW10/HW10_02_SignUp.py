90e736a67c143f6e3987864f11dba0e15cc94baa
SepehrBazyar <bazyar.sepehr@gmail.com>
2021-05-25 00:18:28 +0430

    Finished Q2 Save and Load Users List from JSON File

--- HW10/HW10_02_SignUp.py
+++ HW10/HW10_02_SignUp.py
@@ -11,18 +11,27 @@ class SignUp:
         assert phone_validation(phone), "Start with 09 or +989 and 9 Digits"
         assert email_validation(email), "Validate Sample: name@mail.com"
         self.name, self.phone, self.email = name, phone, email
-        self.USERS.append({'name': name, 'phone': phone, 'email': email})
+        self.USERS.append(self)
 
     @classmethod
     def save(cls, path="users.json"):
+        users_list = []
+        for user in cls.USERS:
+            users_list.append({
+                "name": user.name,
+                "phone": user.phone,
+                "email": user.email
+            })
         with open(path, 'w') as fl:
-            json.dump(cls.USERS, fl, indent=4)
+            json.dump(users_list, fl, indent=4)
 
     @classmethod
     def load(cls, path="users.json"):
         try:
-            with open(path, 'r') as fl:
-                cls.USERS = json.load(fl)
+            with open(path) as fl:
+                users_list = json.load(fl)
+            for user in users_list:
+                SignUp(user["name"], user["phone"], user["email"])
         except:
             pass
 
