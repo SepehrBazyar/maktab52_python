bc776c6b83432206300749ce08aa1bccf1c04e8f
SepehrBazyar <bazyar.sepehr@gmail.com>
2021-05-25 17:01:35 +0430

    Write Comment for Code

--- HW10/HW10_04_Beautifulsoup.py
+++ HW10/HW10_04_Beautifulsoup.py
@@ -8,12 +8,12 @@ logging.basicConfig(level=logging.INFO,
 
 URL = "https://www.pcmag.com/how-to/google-assistant-tips"
 resp = get(URL)
-if resp.ok:
-    soup = BeautifulSoup(resp.text, 'html.parser')
-    print(soup.prettify(encoding='utf-8'))
+if resp.ok:  # status code of response less than 400 successfully
+    soup = BeautifulSoup(resp.text, 'html.parser')  # create instance
+    print(soup.prettify(encoding='utf-8'))  # show good format text
     p_tags = [paragraph.get_text() for paragraph in soup.find_all('p')]
     with open("PCMAG.txt", 'w') as fl:
-        print(*p_tags, sep='\n', file=fl)
+        print(*p_tags, sep='\n', file=fl)  # p_tags deleted <p> tags
     logging.info("Paragraphs Saved at PCMAG.txt File")
 else:
     logging.error(f"HTTP Status Code: {resp.status_code}")
