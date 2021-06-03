ba1bc4ef07509ebe9787d0c1b80a224d44e39b03
SepehrBazyar <bazyar.sepehr@gmail.com>
2021-06-03 23:20:16 +0430

    Finished Q2 with CURL & Parsing JSON with the Python Module

--- HW11/HW11_02_Sun.sh
+++ HW11/HW11_02_Sun.sh
@@ -2,4 +2,12 @@
 #Written by: Sepehr Bazyar
 read -p "Please Enter the Latitude  in Decimal Degrees(Tehran = 35.6961): " lat
 read -p "Please Enter the Longitude in Decimal Degrees(Tehran = 51.4231): " lng
-curl "https://api.sunrise-sunset.org/json?lat=$lat&lng=$lng"
+curl "https://api.sunrise-sunset.org/json?lat=$lat&lng=$lng" > "Sun.json"
+
+if [ $(cat "Sun.json" | python -m json.tool | grep '\"status\"' | cut -d ':' -f 2) == '"OK"' ];
+then
+    echo "Tolou Aftab: $(cat "Sun.json" | python -m json.tool | grep '\"sunrise\"' | cut -d ':' -f 2-4 | cut -c3-12)"
+    echo "Qorub Aftab: $(cat "Sun.json" | python -m json.tool | grep '\"sunset\"' | cut -d ':' -f 2-4 | cut -c3-12)"
+else
+    echo "INVALID_REQUEST: Indicates that Either lat or lng Parameters are Missing or Invalid!"
+fi
