0b0d4541ae40b14932765a3ed4b9f61a4c56dd8e
SepehrBazyar <bazyar.sepehr@gmail.com>
2021-06-03 20:56:25 +0430

    Request to Site Successfully now Parse JSON File

--- HW11/HW11_02_Sun.sh
+++ HW11/HW11_02_Sun.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 #Written by: Sepehr Bazyar
-read -p "Please Enter the File Name: " filename
+read -p "Please Enter the Latitude  in Decimal Degrees(Tehran = 35.6961): " lat
+read -p "Please Enter the Longitude in Decimal Degrees(Tehran = 51.4231): " lng
+curl "https://api.sunrise-sunset.org/json?lat=$lat&lng=$lng"
