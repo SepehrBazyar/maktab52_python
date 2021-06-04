09becf8375413523c274ebf7e0339a6fd28841c1
SepehrBazyar <bazyar.sepehr@gmail.com>
2021-06-04 20:39:37 +0430

    Write Basic Init of DataBase Connection

--- HW11/HW11_03_DBManager.py
+++ HW11/HW11_03_DBManager.py
@@ -1,8 +1,10 @@
 # Written by: Sepehr Bazyar
-import psycopg2
-import logging
+from psycopg2 import connect
+from psycopg2._psycopg import connection, cursor
 
 from abc import ABC, abstractmethod
+
+import logging
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)-10s - %(message)s')
@@ -23,6 +25,20 @@ class BaseManager(ABC):
 
 
 class DatabaseManager(BaseManager):
+    TABLES = {}
+
+    def __init__(self, database: str, user: str, password: str, host: str, port: str) -> None:
+        try:
+            self.conn: connection = connect(
+                database=database, user=user, password=password, host=host, port=port)
+            self.cursor: cursor = self.conn.cursor
+            logging.info(f"{__name__}: Connection Succeeded.")
+        except:
+            logging.error(f"{__name__}: Connection Failed.")
+
+    def __call__(self, *args, **kwds):
+        return super().__call__(*args, **kwds)
+
     def create(self):
         pass
 
@@ -34,3 +50,7 @@ class DatabaseManager(BaseManager):
 
     def delete(self):
         pass
+
+    def __del__(self):
+        self.conn.close()
+        logging.warning(f"{__name__}: Data Base Closed.")
