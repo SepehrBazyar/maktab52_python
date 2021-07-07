f808cdbddeede544ea0c9e80f1abecd307e0a14e
SepehrBazyar <bazyar.sepehr@gmail.com>
2021-07-07 18:38:59 +0430

    write starting calc price func by use calc percent func

--- HW13/MongoDB.py
+++ HW13/MongoDB.py
@@ -1,7 +1,6 @@
 # Written by: Sepehr Bazyar
 from pymongo import MongoClient
 from company_data import *
-from pprint import pprint
 
 import logging
 
@@ -9,19 +8,27 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)-10s -
 
 def calculate_markup_percent(product_type: str, count: int) -> float:
     """
-    Query to Calculate Markup of Product by Percent Unit\n
-    Markup for 1 Count Lower Cost and Greater than Lower Count is Equal Upper Cost\n
-    Other Count Liner. For Example: calculate_markup_percent("1", 5) = 14.44
+    Query to Calculate Markup of Product by Percent Unit.\n
+    Markup for 1 Count Lower Cost and Greater than Lower Count is Equal Upper Cost.\n
+    Other Count Calculate Liner. For Example: calculate_markup_percent("1", 5) = 14.44
     """
 
     query = {"product_type": product_type}
     res = markups.find_one(query, {"_id": 0, "lower_cost": 1, "upper_cost": 1, "lower_count": 1})
     m = (res["upper_cost"] - res["lower_cost"]) / (res["lower_count"] - 1)
     h = res["upper_cost"] - (m * res["lower_count"])
-    return float(round(m * count + h, 2))
+    return float(round(m * count + h, 2)) if count < res["lower_count"] else float(res["upper_cost"])
 
-def calculate_product_price():
-    pass
+def calculate_product_price(product_type: str, count: int, userid: int = None) -> float:
+    """
+    Query to Calculate Price of Product by Commission User in Dollar Unit.\n
+    It is Possible to Buy for Everyone so userid Parametr is Optional.
+    """
+
+    query = {"type": product_type}
+    prod = products.find_one(query, {"_id": 0, "price": 1, "commission_groups": 1})
+    ans = prod["price"] * (1 - calculate_markup_percent(product_type, count) / 100)
+    return ans
 
 client = MongoClient('mongodb://localhost:27017/')
 db = client.company
